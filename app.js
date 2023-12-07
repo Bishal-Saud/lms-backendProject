9e1e38878f812935e67951daf8139e68e7456763
Bishal-Saud <115933710+Bishal-Saud@users.noreply.github.com>
2023-12-07 12:39:58 +0545

    trying to solve cors errors 1

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const app = express();
 app.use(
   cors({
     origin: process.env.FRONTEND_URL,
-    credentials: true,
+    // credentials: true,
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
     allowedHeaders: "Content-Type,Authorization",
   })
